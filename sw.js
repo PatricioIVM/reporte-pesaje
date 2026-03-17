a19490c4f1412e12477b8fc00ce98d281d3dae3f
PatricioIVM <pvalenzuelamadariaga@gmail.com>
2026-03-17 19:47:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var CACHE = "reporte-pesaje-v1";
-var FILES = ["/reporte-pesaje/", "/reporte-pesaje/index.html", "/reporte-pesaje/manifest.json"];
+var FILES = ["https://patricioivm.github.io/reporte-pesaje/", "https://patricioivm.github.io/reporte-pesaje/index.html", "https://patricioivm.github.io/reporte-pesaje/manifest.json"];
 
 self.addEventListener("install", function(e) {
   e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
