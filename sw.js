2639f85352b1ed9204b4e88bf9cd1da90ed6a60a
PatricioIVM <pvalenzuelamadariaga@gmail.com>
2026-03-17 19:31:06 -0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,12 @@
+var CACHE = "reporte-pesaje-v1";
+var FILES = ["/reporte-pesaje/", "/reporte-pesaje/index.html", "/reporte-pesaje/manifest.json"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) {
+    return r || fetch(e.request);
+  }));
+});
